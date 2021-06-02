fa5ba9452f82ed5291f79c8c46d795e21c69043c
Ellie Banks <elliebanks@ellies-mbp.home>
2021-06-02 08:45:19 -0500

    updated

--- with-jQuery/app.js
+++ with-jQuery/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let btn = $('<button id="newBtn">issa button with a NICE message</button>');
+    let btn = $('<button id="newBtn">Click me for a NICE message</button>');
     $('body').append(btn);
 
     let $submitBtn = $('#showInput');
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         //create a div in html
-        let myDiv = $('<div id="newDiv">issa div</div>');
+        
+        let myDiv = $('<div id="newDiv">this a div</div>');
         $('body').append(myDiv);
 
         //make div background change when hovered (without using hover)
@@ -33,9 +34,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //create a paragraph with some text
         //make the paragraph text color red on click...and then comment it out.
-        let p = $('<p></p>');
+        let p = $('<p id="para"></p>');
         $('body').append(p);
-        $(p).text("this is a paragraph. i made it with jquery. i'm tired and want to take a nap, but i have to finish this lab. plz click me.");
+        $(p).text("Roses are red, violets are blue. Please click me and I will change hue.");
 
         /*$(p).click(function () {
             $(p).css("color", "red");
@@ -66,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         //make a button and an empty div
-        let btn2 = $('<button>hello i forgot my name</button>');
+        let btn2 = $('<button id="btn2">hello i forgot my name</button>');
         $('body').append(btn2)
         let div2 = $('<div></div>');
         $('body').append(div2);
 
         //when button is clicked create a span with your name & place in the empty div
         btn2.click(function () {
-            let span = $('<span>');
+            let span = $('<span id="myName">');
             span.text('oh yes, it is ellen banks');
             (div2).append(span);
         });
@@ -91,6 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "julius caesar",
             "katy perry's baby",
         ];
+        
         
         let i = 0;
         //when html btn3 is clicked, each friend's name should appear as a list item in the ul
